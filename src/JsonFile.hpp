6292a7422aca19cc05938238ab57aab4dc3f3377
Tom Reed <t.reed1104@gmail.com>
2019-01-14 10:59:03 +0000

    Implemented the loading json file and the test function for printing the contents

--- src/JsonFile.hpp
+++ src/JsonFile.hpp
@@ -1,10 +1,11 @@
-#ifndef ARKENGINE_ANIMATION_H_
-#define ARKENGINE_ANIMATION_H_
+#ifndef PARSEJSON_JSONFILE_H_
+#define PARSEJSON_JSONFILE_H_
 
-#include "rapidjson/document.h"
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "rapidjson/document.h"
+#include <rapidjson/istreamwrapper.h>
 
 class JsonFile {
 public:
@@ -19,9 +20,11 @@ public:
 
 
 private:
+	rapidjson::Document* jsonDocument;
 	std::string rawFileText;
 
 	bool Load(const std::string& fileName);
+	void enterObject(const rapidjson::Value &obj, size_t indent = 0);
 
 };
 
@@ -34,28 +37,88 @@ JsonFile::JsonFile(const std::string& fileName) {
 		std::cout << "JsonFile.hpp >>>> File name was not supplied, no file was loaded" << std::endl;
 	}
 }
-
 JsonFile::~JsonFile() {
-
+	delete jsonDocument;
 }
 
 bool JsonFile::Load(const std::string& fileName) {
-	std::ifstream file;
-	file.open(fileName);
-	if (!file) {
-		std::cout << "JsonFile.hpp >>>> Error opening file, Json File was not loaded" << std::endl;
-		return false;
-	}
-	char c;
-	while (file >> c) {
-		rawFileText += c;
-	}
 
-
+	std::ifstream fileStream(fileName);
+	rapidjson::IStreamWrapper inputStream(fileStream);
+	jsonDocument = new rapidjson::Document();
+	jsonDocument->ParseStream(inputStream);
+	
 	std::cout << "JsonFile.hpp >>>> File was not loaded" << std::endl;
 	return false;
 }
 
+void JsonFile::enterObject(const rapidjson::Value & obj, size_t indent) {
+	if (obj.IsObject()) {
+		for (rapidjson::Value::ConstMemberIterator itr = obj.MemberBegin(); itr != obj.MemberEnd(); ++itr) {   //iterate through object   
+			const rapidjson::Value& objName = obj[itr->name.GetString()];
+			for (size_t i = 0; i != indent; ++i) {
+				std::cout << " ";
+			}
+
+			std::cout << itr->name.GetString() << ": "; //key name
+
+			if (itr->value.IsNumber()) {
+				std::cout << itr->value.GetInt();
+			}
+			else if (itr->value.IsString()) {
+				std::cout << itr->value.GetString();
+			}
+			else if (itr->value.IsBool()) {
+				std::cout << itr->value.GetBool();
+			}
+			else if (itr->value.IsArray()) {
+				bool isFirst = true;
+				for (rapidjson::SizeType i = 0; i < itr->value.Size(); i++) {
+					if (itr->value[i].IsNumber()) {
+						std::cout << itr->value[i].GetInt();
+					}
+					else if (itr->value[i].IsString()) {
+						std::cout << itr->value[i].GetString();
+					}
+					else if (itr->value[i].IsBool()) {
+						std::cout << itr->value[i].GetBool();
+					}
+					else if (itr->value[i].IsObject()) {
+
+						const rapidjson::Value& m = itr->value[i];
+						for (auto& v : m.GetObject()) {
+							if (isFirst) {
+								std::cout << "\n";
+								isFirst = false;
+							}
+							if (m[v.name.GetString()].IsNumber()) {
+								std::cout << v.name.GetString() << ": " << m[v.name.GetString()].GetInt();
+							}
+							else if (m[v.name.GetString()].IsString()) {
+								std::cout << v.name.GetString() << ": " << m[v.name.GetString()].GetString();
+							}
+							else if (m[v.name.GetString()].IsBool()) {
+								std::cout << v.name.GetString() << ": " << m[v.name.GetString()].GetBool();
+							}
+							else  if (m[v.name.GetString()].IsArray()) {
+								std::cout << "Nested Array";
+							}
+							else  if (m[v.name.GetString()].IsObject()) {
+								for (size_t i = 0; i != indent + 4; ++i) {
+									std::cout << " ";
+								}
+								std::cout << v.name.GetString() << ": " << std::endl;
+								enterObject(m[v.name.GetString()], indent + 8);
+							}
+						}
+					}
+				}
+			}
+			std::cout << std::endl;
+			enterObject(objName, indent + 4);
+		}
+	}
+}
 
 // Getters
 template<typename T> T JsonFile::Get(const std::string& objectName) {
@@ -71,7 +134,6 @@ template<> std::string JsonFile::Get(const std::string& objectName) {
 template<> bool JsonFile::Get(const std::string& objectName) {
 	return false;
 }
-
 
 // Setters
 template<typename T> void JsonFile::Set(const std::string & objectName, const T & value) {
