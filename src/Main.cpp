dbcf30a3a58299f90dc599101686d66cd4ed1249
Tom Reed <t.reed1104@gmail.com>
2019-01-16 09:49:05 +0000

    Added the tests for float and double gets

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,8 +2,11 @@
 
 int main() {
 	JsonFile testFile = JsonFile("content/engine.json");
-	int windowScaleX = testFile.Get<int>("engine.window.scalar.x");
+	int windowScaleX = testFile.Get<int>("engine.window.grid size.width");
 	int keyX = testFile.Get<int>("engine.key bindings.1.binding.key value");
+
+	float versionF = testFile.Get<float>("engine.window.scalar.x");
+	double versionD = testFile.Get<double>("engine.window.scalar.y");
 
 	return 0;
 }
