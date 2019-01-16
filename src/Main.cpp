bd8415b573a1ba231588dcdb87b7346b71607902
Tom Reed <t.reed1104@gmail.com>
2019-01-16 09:54:08 +0000

    Cleaned up the testing function calls

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,13 +1,15 @@
 #include "JsonFile.hpp"
 
 int main() {
+	// Load the File
 	JsonFile testFile = JsonFile("content/engine.json");
-	int windowScaleX = testFile.Get<int>("engine.window.grid size.width");
-	int keyX = testFile.Get<int>("engine.key bindings.1.binding.key value");
 
-	float versionF = testFile.Get<float>("engine.window.scalar.x");
-	double versionD = testFile.Get<double>("engine.window.scalar.y");
-	std::string developerName = testFile.Get<std::string>("engine.developer");
+	// Get Tests
+	int getIntTest = testFile.Get<int>("engine.window.grid size.width");
+	float getFloatTest = testFile.Get<float>("engine.window.scalar.x");
+	double getDoubleTest = testFile.Get<double>("engine.window.scalar.y");
+	std::string getStringTest = testFile.Get<std::string>("engine.developer");
 
+	// Close the test program
 	return 0;
 }
