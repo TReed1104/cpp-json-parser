06c680e6e0ab3e8303fde718c48a347956bff2bf
Tom Reed <t.reed1104@gmail.com>
2019-01-16 10:01:21 +0000

    Added the bool test to main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,6 +9,7 @@ int main() {
 	float getFloatTest = testFile.Get<float>("engine.game controller.thumb stick dead zone");
 	double getDoubleTest = testFile.Get<double>("engine.game controller.trigger dead zone");
 	std::string getStringTest = testFile.Get<std::string>("engine.developer");
+	bool getBoolTest = testFile.Get<bool>("engine.vsync");
 
 	// Close the test program
 	return 0;
