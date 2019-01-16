6fec5e0830fd861f48955918dcc3b0ffae509704
Tom Reed <t.reed1104@gmail.com>
2019-01-16 09:12:09 +0000

    Added a value test and a array indexing test to the main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,7 +2,8 @@
 
 int main() {
 	JsonFile testFile = JsonFile("content/engine.json");
-	std::string programId = testFile.Get<std::string>("engine.program id");
+	int windowScaleX = testFile.Get<int>("engine.window.scalar.x");
+	int keyX = testFile.Get<int>("engine.key bindings.1.binding.key value");
 
 	return 0;
 }
