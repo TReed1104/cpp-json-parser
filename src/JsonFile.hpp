2c2707e60084bb38852cc25d317153170358339e
Tom Reed <t.reed1104@gmail.com>
2019-01-16 09:33:26 +0000

    Implemented the get double function

--- src/JsonFile.hpp
+++ src/JsonFile.hpp
@@ -152,10 +152,33 @@ template<> inline float JsonFile::Get(const std::string& objectName) {
 	return returnValue;
 }
 template<> inline double JsonFile::Get(const std::string& objectName) {
-	// this gives us the stack of node names to use to traverse the json file's structure, e.g. root.head.value
-	std::vector<std::string> splitString = SplitString(objectName, '.');
+	std::cout << "JsonFile.hpp >>>> File: " << fileName << " Get<double> - Target: '" << objectName << "'" << std::endl;
+	std::vector<std::string> splitString = SplitString(objectName, '.');	// this gives us the stack of node names to use to traverse the json file's structure, e.g. root.head.value
+	double returnValue = NULL;
 
-	return 0.0;
+	std::cout << "JsonFile.hpp >>>> File: " << fileName << " Get<double> - Accessing object/key: '" << splitString[0] << "'" << std::endl;
+	rapidjson::Value* value = &(*jsonDocument)[splitString[0].c_str()];
+	const size_t sizeOfSplitString = splitString.size();
+	for (size_t i = 1; i < sizeOfSplitString; i++) {
+		if (!value->IsArray()) {
+			std::cout << "JsonFile.hpp >>>> File: " << fileName << " Get<double> - Accessing object/key: '" << splitString[i] << "'" << std::endl;
+			value = &(*value)[splitString[i].c_str()];
+		}
+		else {
+			std::cout << "JsonFile.hpp >>>> File: " << fileName << " Get<double> - Accessing Array element: '" << splitString[i] << "'" << std::endl;
+			int index = std::stoi(splitString[i]);
+			value = &(*value)[index];
+		}
+	}
+	if (!value->IsObject()) {
+		returnValue = value->GetDouble();
+	}
+	else {
+		std::cout << "JsonFile.hpp >>>> File: " << fileName << " Get<double> tried to return an object" << std::endl;
+		return NULL;
+	}
+
+	return returnValue;
 }
 template<> inline std::string JsonFile::Get(const std::string& objectName) {
 	// this gives us the stack of node names to use to traverse the json file's structure, e.g. root.head.value
