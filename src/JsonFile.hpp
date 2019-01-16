7cb60097736f8503748089d3f87981575f1fd484
Tom Reed <t.reed1104@gmail.com>
2019-01-16 09:11:51 +0000

    Implemented a base version of the get int value function

--- src/JsonFile.hpp
+++ src/JsonFile.hpp
@@ -92,10 +92,34 @@ template<typename T> inline T JsonFile::Get(const std::string& objectName) {
 	return 0;
 }
 template<> inline int JsonFile::Get(const std::string& objectName) {
+	std::cout << "JsonFile.hpp >>>> File: " << fileName << " Get<int> - Target: '" << objectName << "'" << std::endl;
 	// this gives us the stack of node names to use to traverse the json file's structure, e.g. root.head.value
 	std::vector<std::string> splitString = SplitString(objectName, '.');
+	int returnValue = NULL;
 
-	return 0;
+	std::cout << "JsonFile.hpp >>>> File: " << fileName << " Get<int> - Accessing object/key: '" << splitString[0] << "'" << std::endl;
+	rapidjson::Value* value = &(*jsonDocument)[splitString[0].c_str()];
+	const size_t sizeOfSplitString = splitString.size();
+	for (size_t i = 1; i < sizeOfSplitString; i++) {
+		if (!value->IsArray()) {
+			std::cout << "JsonFile.hpp >>>> File: " << fileName << " Get<int> - Accessing object/key: '" << splitString[i] << "'" << std::endl;
+			value = &(*value)[splitString[i].c_str()];
+		}
+		else {
+			std::cout << "JsonFile.hpp >>>> File: " << fileName << " Get<int> - Accessing Array element: '" << splitString[i] << "'" << std::endl;
+			int index = std::stoi(splitString[i]);
+			value = &(*value)[index];
+		}
+	}
+	if (!value->IsObject()) {
+		returnValue = value->GetInt();
+	}
+	else {
+		std::cout << "JsonFile.hpp >>>> File: " << fileName << " Get<int> tried to return an object" << std::endl;
+		return NULL;
+	}
+
+	return returnValue;
 }
 template<> inline float JsonFile::Get(const std::string& objectName) {
 	// this gives us the stack of node names to use to traverse the json file's structure, e.g. root.head.value
@@ -116,7 +140,7 @@ template<> inline bool JsonFile::Get(const std::string& objectName) {
 	return false;
 }
 
-// Setters
+// Setters= 
 template<typename T> inline void JsonFile::Set(const std::string & objectName, const T & value) {
 	std::cout << "JsonFile.hpp >>>> Default Template for Set, type specific ones should get called instead" << std::endl;
 	return 0;
