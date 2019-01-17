5214ce7ed92b2c2b63faa3a356c6ba6a5893b8e0
Tom Reed <t.reed1104@gmail.com>
2019-01-17 10:21:12 +0000

    Added a new json file and tests (this is a level file from ark-engine, testing the new configuration)

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,6 +11,10 @@ int main() {
 	std::string getStringTest = testFile.Get<std::string>("engine.developer");
 	bool getBoolTest = testFile.Get<bool>("engine.vsync");
 
+	JsonFile arrayTextFile = JsonFile("content/test_level.json");
+	int tileReferenceTest = arrayTextFile.Get<int>("level.tile grid.156");
+	
+
 	// Close the test program
 	return 0;
 }
