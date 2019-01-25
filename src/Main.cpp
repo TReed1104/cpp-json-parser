0c98f5f93320fff57633fef8dc23d50156a89c20
Tom Reed <t.reed1104@gmail.com>
2019-01-25 10:49:31 +0000

    Renamed the JsonFile variable for the Get Tests for clarity

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,21 +2,21 @@
 
 int main() {
 	// Load the File
-	JsonFile testFile = JsonFile("content/get_test.json");
+	JsonFile testFileForGets = JsonFile("content/get_test.json");
 	
 	// Get<T>() Tests
-	int getIntTest = testFile.Get<int>("value test.int");
-	float getFloatTest = testFile.Get<float>("value test.float");
-	double getDoubleTest = testFile.Get<double>("value test.double");
-	std::string getStringTest = testFile.Get<std::string>("value test.string");
-	bool getBoolTest = testFile.Get<bool>("value test.boolean");
+	int getIntTest = testFileForGets.Get<int>("value test.int");
+	float getFloatTest = testFileForGets.Get<float>("value test.float");
+	double getDoubleTest = testFileForGets.Get<double>("value test.double");
+	std::string getStringTest = testFileForGets.Get<std::string>("value test.string");
+	bool getBoolTest = testFileForGets.Get<bool>("value test.boolean");
 
 	// GetArray<T>() Tests
-	std::vector<int> getIntArrayTest = testFile.GetArray<int>("array test.int array");
-	std::vector<float> getFloatArrayTest = testFile.GetArray<float>("array test.float array");
-	std::vector<double> getDoubleArrayTest = testFile.GetArray<double>("array test.double array");
-	std::vector<std::string> getStringArrayTest = testFile.GetArray<std::string>("array test.string array");
-	std::vector<bool> getBoolArrayTest = testFile.GetArray<bool>("array test.boolean array");
+	std::vector<int> getIntArrayTest = testFileForGets.GetArray<int>("array test.int array");
+	std::vector<float> getFloatArrayTest = testFileForGets.GetArray<float>("array test.float array");
+	std::vector<double> getDoubleArrayTest = testFileForGets.GetArray<double>("array test.double array");
+	std::vector<std::string> getStringArrayTest = testFileForGets.GetArray<std::string>("array test.string array");
+	std::vector<bool> getBoolArrayTest = testFileForGets.GetArray<bool>("array test.boolean array");
 	
 	// Close the program
 	return 0;
