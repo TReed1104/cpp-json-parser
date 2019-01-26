c14f2a578adc5b08153b5bc3b3d19d200be73e01
Tom Reed <t.reed1104@gmail.com>
2019-01-26 16:36:40 +0000

    Added a SetArray<bool>() type test

--- src/Main.cpp
+++ src/Main.cpp
@@ -39,14 +39,17 @@ int main() {
 		testIntVector.push_back(i);
 	}
 	testFileForSets.SetArray<int>("array test.int array", testIntVector);
-
 	std::vector<std::string> stringVector;
 	stringVector.push_back("one");
 	stringVector.push_back("two");
 	stringVector.push_back("three");
 	testFileForSets.SetArray<std::string>("array test.string array", stringVector);
+	std::vector<bool> boolvector;
+	boolvector.push_back(true);
+	boolvector.push_back(false);
+	testFileForSets.SetArray<bool>("array test.boolean array", boolvector);
 
-
+	// Insert tests
 	testFileForSets.Insert<int>("", "insert int test", 5);
 	testFileForSets.Insert<int>("", "insert int test", 5);					// Checking if we have already added this key, should produce an error output
 	testFileForSets.Insert<int>("value test", "insert int test", 5);
